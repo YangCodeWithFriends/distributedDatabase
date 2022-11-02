23168865b71406828898491153edf820a11a44ee
YANGZ001 <zyfunnyyoung@gmail.com>
2022-11-02 21:45:23 +0800

    Feature: add query_cluster_load.sh

---
+++ src/main/resources/scripts/query_cluster_load.sh
@@ -0,0 +1,14 @@
+ssh cs4224j@xcnd20.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && uptime'"
+
+ssh cs4224j@xcnd21.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && uptime'"
+
+ssh cs4224j@xcnd22.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && uptime'"
+
+ssh cs4224j@xcnd23.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && uptime'"
+
+ssh cs4224j@xcnd24.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && uptime'"
