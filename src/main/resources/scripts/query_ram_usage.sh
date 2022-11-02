fddc2ed650e36405f2c64c2e3450623c5288ae95
YANGZ001 <zyfunnyyoung@gmail.com>
2022-11-02 18:13:58 +0800

    Feature: add query_ram_usage.sh

---
+++ src/main/resources/scripts/query_ram_usage.sh
@@ -0,0 +1,14 @@
+ssh cs4224j@xcnd20.comp.nus.edu.sg \
+    "sh -c 'cd /mnt/ramdisk && du -h --max-depth=0 *'"
+
+ssh cs4224j@xcnd21.comp.nus.edu.sg \
+    "sh -c 'cd /mnt/ramdisk && du -h --max-depth=0 *'"
+
+ssh cs4224j@xcnd22.comp.nus.edu.sg \
+    "sh -c 'cd /mnt/ramdisk && du -h --max-depth=0 *'"
+
+ssh cs4224j@xcnd23.comp.nus.edu.sg \
+    "sh -c 'cd /mnt/ramdisk && du -h --max-depth=0 *'"
+
+ssh cs4224j@xcnd24.comp.nus.edu.sg \
+    "sh -c 'cd /mnt/ramdisk && du -h --max-depth=0 *'"
