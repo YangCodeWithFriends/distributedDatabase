af6f51c6d383ebba6ce6932c0fccfb00a067f28c
YANGZ001 <zyfunnyyoung@gmail.com>
2022-11-02 21:45:35 +0800

    Feature: add query_threads.sh

---
+++ src/main/resources/scripts/query_threads.sh
@@ -0,0 +1,29 @@
+ssh cs4224j@xcnd20.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && ps -eaf | grep java'"
+
+ssh cs4224j@xcnd20.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && ps -eaf | grep yb'"
+
+ssh cs4224j@xcnd21.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && ps -eaf | grep java'"
+
+ssh cs4224j@xcnd21.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && ps -eaf | grep yb'"
+
+ssh cs4224j@xcnd22.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && ps -eaf | grep java'"
+
+ssh cs4224j@xcnd22.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && ps -eaf | grep yb'"
+
+ssh cs4224j@xcnd23.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && ps -eaf | grep java'"
+
+ssh cs4224j@xcnd23.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && ps -eaf | grep yb'"
+
+ssh cs4224j@xcnd24.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && ps -eaf | grep java'"
+
+ssh cs4224j@xcnd24.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && ps -eaf | grep yb'"
