59c4c080c571e774593891c99eb20f45c7357c1b
YANGZ001 <zyfunnyyoung@gmail.com>
2022-11-02 17:00:09 +0800

    Fix: NPE for PopularItemTransaction, and seperate cql and sql bash script

---
+++ src/main/resources/scripts/start_client_cql.sh
@@ -0,0 +1,18 @@
+# YCQL
+ssh cs4224j@xcnd20.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && rm YCQL-log-*.txt'" &
+
+ssh cs4224j@xcnd20.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && java -cp yugabyte-simple-java-app-1.0-SNAPSHOT.jar SampleApp YCQL 0 &'" &
+
+ssh cs4224j@xcnd21.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && java -cp yugabyte-simple-java-app-1.0-SNAPSHOT.jar SampleApp YCQL 1 &'" &
+
+ssh cs4224j@xcnd22.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && java -cp yugabyte-simple-java-app-1.0-SNAPSHOT.jar SampleApp YCQL 2 &'" &
+
+ssh cs4224j@xcnd23.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && java -cp yugabyte-simple-java-app-1.0-SNAPSHOT.jar SampleApp YCQL 3 &'" &
+
+ssh cs4224j@xcnd24.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && java -cp yugabyte-simple-java-app-1.0-SNAPSHOT.jar SampleApp YCQL 4 &'" &
---
+++ src/main/resources/scripts/start_client_sql.sh
@@ -0,0 +1,18 @@
+# YSQL
+ssh cs4224j@xcnd20.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && rm YSQL-log-*.txt'" &
+
+ssh cs4224j@xcnd20.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && java -cp yugabyte-simple-java-app-1.0-SNAPSHOT.jar SampleApp YSQL 0 &'" &
+
+ssh cs4224j@xcnd21.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && java -cp yugabyte-simple-java-app-1.0-SNAPSHOT.jar SampleApp YSQL 1 &'" &
+
+ssh cs4224j@xcnd22.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && java -cp yugabyte-simple-java-app-1.0-SNAPSHOT.jar SampleApp YSQL 2 &'" &
+
+ssh cs4224j@xcnd23.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && java -cp yugabyte-simple-java-app-1.0-SNAPSHOT.jar SampleApp YSQL 3 &'" &
+
+ssh cs4224j@xcnd24.comp.nus.edu.sg \
+    "sh -c 'cd ~/target && java -cp yugabyte-simple-java-app-1.0-SNAPSHOT.jar SampleApp YSQL 4 &'" &
